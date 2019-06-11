128107677be3691c93d943f7de30e967d7da78ce
Marsden Ricketts <marsdenrickett@gmail.com>
2019-06-11 10:39:14 -0400

    __dir in server

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const db = require("./models")
+const db = require("./models");
+const path = require('path');
 
 
 // set up middleware
@@ -15,19 +16,17 @@ app.use (express.json());
 // turn on routes
 require('./routes/api-routes')(app);
 
-// wildcard (404) error
-app.get("*", function(req, res){
-  res.json({
-    status: 404,
-    message: "Wrong place"
-  });
-});
+
 
 // turn on server
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+})
 
 // // db.sequelize.sync().then(function() {
 //   app.listen(PORT, function() {
